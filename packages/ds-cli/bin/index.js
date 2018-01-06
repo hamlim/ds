78e5e0b0561890cd4347d24efd74f785fe46995f
Matt Hamlin <hamlim@outlook.com>
2018-01-05 20:18:42 -0500

    Pr/local paths - closes #1 (#8)
    * Remolve old cli file, use path.join and current working directory
    
    * v1.3.0
    
    * v1.4.0
    
    * Need to remember the top comment on the script

--- packages/ds-cli/bin/index.js
+++ packages/ds-cli/bin/index.js
@@ -5,6 +5,10 @@ var _fs = require('fs');
 
 var _fs2 = _interopRequireDefault(_fs);
 
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
 var _ds = require('@matthamlin/ds');
 
 var _ds2 = _interopRequireDefault(_ds);
@@ -12,10 +16,11 @@ var _ds2 = _interopRequireDefault(_ds);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const main = () => {
+  const cwd = process.cwd();
   _fs2.default.writeFile(process.argv[5], (0, _ds2.default)({
-    fonts: require(process.argv[2]),
-    colors: require(process.argv[3]),
-    numbers: require(process.argv[4])
+    fonts: require(_path2.default.join(cwd, process.argv[2])),
+    colors: require(_path2.default.join(cwd, process.argv[3])),
+    numbers: require(_path2.default.join(cwd, process.argv[4]))
   }), err => {
     console.error(err);
   });
