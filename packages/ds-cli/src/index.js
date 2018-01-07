6e2d48a6765946d4eaf4e1053496dfe8c2c201eb
Matt Hamlin <hamlim@outlook.com>
2018-01-06 19:19:25 -0500

    Initial modular scale work
    * module scale initial work
    
    * Use local build cli
    
    * Object rest spread support
    
    * Build cli
    
    * Build font-sizes from ratio
    
    * Add back comment to auto-built cli code

--- packages/ds-cli/src/index.js
+++ packages/ds-cli/src/index.js
@@ -1,18 +1,17 @@
-import fs from 'fs';
-import path from 'path';
-import ds from '@matthamlin/ds';
+import fs from 'fs'
+import path from 'path'
+import ds from '@matthamlin/ds'
 
 const main = () => {
-  const cwd = process.cwd();
+  const cwd = process.cwd()
   fs.writeFile(
-    process.argv[5],
-    ds(
-      {
-        fonts: require(path.join(cwd, process.argv[2])),
-        colors: require(path.join(cwd, process.argv[3])),
-        numbers: require(path.join(cwd, process.argv[4]))
-      }
-    ),
+    process.argv[6],
+    ds({
+      fonts: require(path.join(cwd, process.argv[2])),
+      colors: require(path.join(cwd, process.argv[3])),
+      numbers: require(path.join(cwd, process.argv[4])),
+      modularScale: require(path.join(cwd, process.argv[5])),
+    }),
     err => {
       console.error(err)
     },
--- packages/ds/src/index.js
+++ packages/ds/src/index.js
@@ -1,4 +1,17 @@
-import prettier from 'prettier';
+import prettier from 'prettier'
+
+const stringifyWithUnits = units => dict => key => {
+  if (typeof dict[key] === 'string') {
+    return `"${key}": "${dict[key]}${units}"`
+  } else if (typeof dict[key] === 'number') {
+    return `"${key}": "${dict[key]}${units}"`
+  } else {
+    return `"${key}": {
+      ${Object.keys(dict[key]).map(stringifyWithUnits(units)(dict[key]))}
+    }`
+  }
+}
+
 const stringify = dict => key => {
   if (typeof dict[key] === 'string') {
     return `"${key}": "${dict[key]}"`
@@ -9,7 +22,16 @@ const stringify = dict => key => {
   }
 }
 
-export default ({ fonts, colors, numbers }) => prettier.format(`{
+export default ({ fonts, colors, numbers, modularScale, fontSizes = 10 }) => {
+  let sizes = {}
+  if (modularScale) {
+    sizes = Array.from({ length: fontSizes })
+      .map((_, i) => i)
+      .reduce((acc, size, i) => ({ ...acc, [i]: i * modularScale.ratio || 1 }), {})
+  }
+
+  return prettier.format(
+    `{
   "colors": {
     ${Object.keys(colors)
       .map(stringify(colors))
@@ -18,11 +40,19 @@ export default ({ fonts, colors, numbers }) => prettier.format(`{
   "fonts": {
     ${Object.keys(fonts)
       .map(stringify(fonts))
-      .join(',')}
+      .join(',')},
+    "sizes": {
+      ${Object.keys(sizes)
+        .map(stringifyWithUnits('rem')(sizes))
+        .join(',')}
+    }
   },
   "numbers": {
     ${Object.keys(numbers)
       .map(stringify(numbers))
       .join(',')}
   }
-}`, {parser: 'json'});
+}`,
+    { parser: 'json' },
+  )
+}
