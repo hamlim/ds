e672cd68e717c3ab143ee2bbc36eced909a1c46e
Matt Hamlin <hamlim@outlook.com>
2018-01-06 19:11:59 -0500

    Object rest spread support

--- packages/ds/dist/index.js
+++ packages/ds/dist/index.js
@@ -4,11 +4,25 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _prettier = require('prettier');
 
 var _prettier2 = _interopRequireDefault(_prettier);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const stringifyWithUnits = units => dict => key => {
+  if (typeof dict[key] === 'string') {
+    return `"${key}": "${dict[key]}${units}"`;
+  } else if (typeof dict[key] === 'number') {
+    return `"${key}": "${dict[key]}${units}"`;
+  } else {
+    return `"${key}": {
+      ${Object.keys(dict[key]).map(stringifyWithUnits(units)(dict[key]))}
+    }`;
+  }
+};
 
 const stringify = dict => key => {
   if (typeof dict[key] === 'string') {
@@ -20,14 +34,24 @@ const stringify = dict => key => {
   }
 };
 
-exports.default = ({ fonts, colors, numbers }) => _prettier2.default.format(`{
+exports.default = ({ fonts, colors, numbers, modularScale, fontSizes = 10 }) => {
+  let sizes = {};
+  if (modularScale) {
+    sizes = Array.from({ length: fontSizes }).map((_, i) => i).reduce((acc, size, i) => _extends({}, acc, { [i]: i * modularScale.ratio || 1 }));
+  }
+
+  return _prettier2.default.format(`{
   "colors": {
     ${Object.keys(colors).map(stringify(colors)).join(',')}
   },
   "fonts": {
-    ${Object.keys(fonts).map(stringify(fonts)).join(',')}
+    ${Object.keys(fonts).map(stringify(fonts)).join(',')},
+    sizes: {
+      ${Object.keys(sizes).map(stringifyWithUnits('rem')(sizes)).join(',')}
+    }
   },
   "numbers": {
     ${Object.keys(numbers).map(stringify(numbers)).join(',')}
   }
 }`, { parser: 'json' });
+};
